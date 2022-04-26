ec232dc9d29890af7d7d53afee6ffa06435df422
happygoalvin <alvin.yo@trentglobal.edu.sg>
2022-04-26 08:29:13 +0000

    successfully added M:M for cameras_classifications

---
+++ migrations/20220426081601-cameras-classifications.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.createTable('cameras_classifications', {
+    id: {
+      type: 'int',
+      primaryKey: true,
+      autoIncrement: true,
+      unsigned: true,
+      notNull: true
+    },
+    camera_id: {
+      type: 'int',
+      notNull: true,
+      unsigned: true,
+      foreignKey: {
+        name: 'cameras_classifications_camera_fk',
+        table: 'cameras',
+        mapping: 'id',
+        rules: {
+          onDelete: 'cascade',
+          onUpdate: 'restrict'
+        }
+      }
+    },
+    tag_id: {
+      type: 'int',
+      notNull: true,
+      unsigned: true,
+      foreignKey: {
+        name: 'cameras_classifications_classification_fk',
+        table: 'classifications',
+        mapping: 'id',
+        rules: {
+          onDelete: 'cascade',
+          onUpdate: 'restrict'
+        }
+      }
+    }
+  })
+};
+
+exports.down = function (db) {
+  return db.dropTable('')
+};
+
+exports._meta = {
+  "version": 1
+};
