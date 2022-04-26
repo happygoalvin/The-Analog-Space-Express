ff16f9537d55907dfb930f9bf438fe90958e22bd
happygoalvin <alvin.yo@trentglobal.edu.sg>
2022-04-26 05:25:25 +0000

    Added camera, type and classifications migration tables

---
+++ migrations/20220426040938-cameras.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.createTable('cameras', {
+    id: {
+      type: 'int',
+      primaryKey: true,
+      autoIncrement: true,
+      unsigned: true
+    },
+    name: {
+      type: 'string',
+      length: 100,
+      notNull: true
+    },
+    stock: {
+      type: 'smallint',
+      unsigned: true,
+      notNull: true,
+    },
+    cost: {
+      type: 'smallint',
+      unsigned: true,
+      notNull: true
+    },
+    created_date: {
+      type: 'date',
+      notNull: true
+    },
+    description: 'text',
+    camera_iso: {
+      type: 'smallint',
+      unsigned: true,
+      notNull: false
+    },
+    shutter_speed: {
+      type: 'string',
+      length: '100',
+      notNull: false
+    },
+    aperture: {
+      type: 'string',
+      length: '30',
+      notNull: false
+    },
+    focal_length: {
+      type: 'string',
+      length: '30',
+      notNull: false
+    },
+    flash: {
+      type: 'boolean',
+      notNull: false
+    },
+    battery: {
+      type: 'string',
+      length: '30',
+      notNull: false
+    },
+    body_color: {
+      type: 'string',
+      length: '30',
+      notNull: false
+    },
+    format: {
+      type: 'string',
+      length: '30',
+      notNull: false
+    },
+    weight: {
+      type: 'string',
+      length: '30',
+      notNull: false
+    }
+  });
+};
+
+exports.down = function (db) {
+  return db.dropTable('cameras')
+};
+
+exports._meta = {
+  "version": 1
+};
