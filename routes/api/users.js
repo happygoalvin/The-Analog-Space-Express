597dff8b881d3c9d39c651579fecc7dae13890f2
happygoalvin <alvin.yo@trentglobal.edu.sg>
2022-05-15 17:43:14 +0000

    debugging refresh

--- routes/api/users.js
+++ routes/api/users.js
@@ -85,6 +85,7 @@ router.get('/profile', checkIfAuthenticatedJWT, async (req, res) => {
 
 
 router.post('/refresh', async (req, res) => {
+    console.log(req.body)
     const refreshToken = req.body.refreshToken;
     if (!refreshToken) {
         res.sendStatus(401);
