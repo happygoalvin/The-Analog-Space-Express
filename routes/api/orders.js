35d0b969018882f9f4e124f551be35faee8809c5
happygoalvin <alvin.yo@trentglobal.edu.sg>
2022-05-22 03:31:51 +0000

    Debuggin orders api

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -9,14 +9,18 @@ const router = express.Router();
 
 router.get('/', checkIfAuthenticatedJWT, async (req, res) => {
     const userId = req.user.id;
+    console.log("verify JWT passed")
     try {
+        console.log("Before getting orders")
         const order = await Order.where({
             user_id: userId
         }).fetchAll({
             require: false,
             withRelated: [camera]
         })
+        console.log("after getting orders")
         if (order) {
+            console.log(order.toJSON())
             res.status(200)
             res.send(order.toJSON())
         } else {
